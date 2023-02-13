2b36d30261e23e707e125d3153b7156a66148f88
Patricio <patoestradac@gmail.com>
2023-02-12 21:21:26 -0500

    Programa con librería Pandas y archivos de datos creado

---
+++ Avance1EvidenciaPython/main.py
@@ -0,0 +1,13 @@
+# import de Pandas
+import pandas as pd
+
+# Se modificó el archivo Main original
+
+def print_hi():
+    print(f'Hello World!')
+
+    data = pd.Series([1,2,3])
+    print(data)
+
+if __name__ == '__main__':
+    print_hi()
